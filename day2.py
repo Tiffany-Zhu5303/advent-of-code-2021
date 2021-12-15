b71a95a55b04f17e70c49f4176d8655927de64ca
Tiffany Zhu <75339798+Tiffany-Zhu5303@users.noreply.github.com>
2021-12-15 04:20:38 +0000

    day 2 part 2

--- day2.py
+++ day2.py
@@ -15,11 +15,11 @@ def clean(file):
       new_list.append(number)
   return new_list
 
-def sum(l):
+#Part 1
+def sum1(l):
   position = 0
   depth = 0
   for x in l:
-    print(x)
     if x[0] == 'forward':
       position = position + int(x[-1])
     elif x[0] == 'down':
@@ -28,6 +28,25 @@ def sum(l):
       depth = depth - int(x[-1])
   return position*depth
 
+#Part 2
+def sum2(l):
+  position = 0
+  depth = 0
+  aim = 0
+  for x in l:
+    if x[0] == 'forward':
+      position = position + int(x[-1])
+      depth = depth + (aim * int(x[-1]))
+    elif x[0] == 'down':
+      aim = aim + int(x[-1])
+    elif x[0] == 'up':
+      aim = aim - int(x[-1])
+  return position*depth
+
 if __name__ == '__main__':
-  print(sum(clean(given)))
-  print(sum(clean(file)))
+  #Part one
+  print(sum1(clean(given)))
+  print(sum1(clean(file)))
+  #Part two
+  print(sum2(clean(given)))
+  print(sum2(clean(file)))
---
+++ day3.py
@@ -0,0 +1,27 @@
+given = ['00100', '11110', '10110', '10111', '10101', '01111', '00111', '11100', '10000', '11001', '00010', '01010']
+
+'''Find power consumption of submarine:
+power consumption = gamma ray * epsilon rate
+gamma ray = most common bit in the corresponding position'''
+
+#incomplete
+'''
+def slots(l):
+  length = len(l[0])
+  numbers = []
+  for x in range(length):
+    numbers.append([])
+  return numbers
+
+def sorting(number):
+  return none
+
+def cleaned(l):
+  data = slots(l)
+  for number in l:
+    break
+  return none
+
+if __name__ == '__main__':
+  print(slots(given))'''
+    
